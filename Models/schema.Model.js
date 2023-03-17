7d60bfab5494908abbcc6706298a57bbd124f955
Prashant Abhishek Lugun <prashantlugun@gmail.com>
2023-03-17 17:32:27 +0530

    Schema Updated By Abhi

--- Models/schema.Model.js
+++ Models/schema.Model.js
@@ -1,10 +1,24 @@
 const mongoose = require('mongoose');
 
-const User = mongoose.model('users', {
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String
-},{timestamps: true});
+const User = mongoose.model('User', {
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
 
 module.exports = User;
