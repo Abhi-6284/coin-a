b04b10abbd97cece189f099cc7f9bfde4ec99191
Prashant Abhishek Lugun <prashantlugun@gmail.com>
2023-03-24 16:41:14 +0530

    Short Code

--- Controllers/api.controller.js
+++ Controllers/api.controller.js
@@ -3,23 +3,15 @@ const bcrypt = require("bcrypt")
 const jwt = require('jsonwebtoken')
 
 exports.getLogout = async (req, res, next) => {
-    // req.session = null // Deletes the cookie.
     req.session.destroy(); // Deletes the session in the database.
     return res.json({ message: "Successfully Logout" })
 }
-exports.getUsers = async (req, res, next) => {
-    try {
-        const token = req.session.token;
-        if (!token) {
-            return res.json({ message: "Token Expired..." });
-        }
+exports.getUsers = async (req, res) => {
+    try {const token = req.session.token;
+        if (!token) {return res.json({ message: "Token Expired..." });}
         try {
-            const data = jwt.verify(token, process.env.JWT_SECRET_KEY);
-            const userDetails = await root.getUserById({id:data.id});
-            return res.status(201).json({ userDetail: userDetails })
-        } catch {
-            return res.sendStatus(403);
-        }
+            return res.status(201).json({ userDetail: await root.getUserById({id:jwt.verify(token, process.env.JWT_SECRET_KEY).id}) })
+        } catch {return res.sendStatus(403);}
     } catch (e) { return res.status(401).json({ message: e.message }) }
 }
 
@@ -29,9 +21,7 @@ exports.postLogin = async (req, res) => {
         if (!userData) { throw new Error("No User found!.."); } else {
             const token = jwt.sign({ id: userData._id }, process.env.JWT_SECRET_KEY, { expiresIn: process.env.JWT_EXPIRES_IN })
             if (await bcrypt.compare(req.body.password, userData.password)) {
-                console.log(token);
                 req.session.token = token;
-                console.log(req.session);
                 return res.cookie("access_token", token).status(201).json({ message: "Logged in successfully 😊 👌" })
 
             } else { throw new Error("Invalid Password"); }
@@ -48,9 +38,7 @@ exports.postRegister = async (req, res) => {
                 const hashedPassword = await bcrypt.hash(req.body.password, salt);
                 const created = await root.createUser({ firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, password: hashedPassword });
                 const userDetail = await root.getUserByPara({ email: req.body.email });
-                if (created) {
-                    return res.status(200).json({ message: "Registered successfully 😊 👌\nYou are Welcome " + userDetail.firstName + " " + userDetail.lastName })
-                }
+                if (created) {return res.status(200).json({ message: "Registered successfully 😊 👌\nYou are Welcome " + userDetail.firstName + " " + userDetail.lastName })}
             } else { throw new Error("You are existing Users " + userData.email); }
         } else { throw new Error("Your Passwords are not Match"); }
     } catch (e) { return res.status(401).json({ message: e.message }) }
