2dff1f0817d96ce96227299dc8b907070274abe9
Prashant Abhishek Lugun <prashantlugun@gmail.com>
2023-04-05 12:47:07 +0530

    rewrite

---
+++ Middleware/auth.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+exports.verifyToken = (req, res, next) => {
+    // const token = req.headers["authorization"];
+    const token = req.cookies['access_token'];
+    try {
+        if (token){
+            jwt.verify(token, process.env.JWT_SECRET_KEY, (err, valid)=>{
+                if (err){
+                    throw new Error("Please provide a valid token");
+                }else{
+                    next();
+                }
+            })
+        }else{
+            throw new Error("Please provide a token");
+        }
+    } catch (e) {return res.status(401).json({message: e.message})}
+    // next();
+}
--- Routes/api.Router.js
+++ Routes/api.Router.js
@@ -1,5 +1,23 @@
-const router = require('express').Router(), controller = require('../Controllers/api.controller'), postRoutes = { '/login': [controller.postLogin], '/register': [controller.postRegister] }
-for (const [path, handlers] of Object.entries(postRoutes)) { router.post(path, handlers) }
-const getRoutes = { '/user': [controller.getUsers], '/logout': [controller.getLogout] }
-for (const [path, handlers] of Object.entries(getRoutes)) { router.get(path, handlers); }
+const router = require('express').Router();
+const controller = require('../Controllers/api.controller');
+const { verifyToken } = require('../Middleware/auth');
+
+const postRoutes = { 
+    '/login': [controller.postLogin],
+    '/register': [controller.postRegister] 
+}
+
+for (const [path, handlers] of Object.entries(postRoutes)) { 
+    router.post(path, handlers) 
+}
+
+const getRoutes = { 
+    '/user': [controller.getUsers], 
+    '/logout': [controller.getLogout] 
+}
+
+for (const [path, handlers] of Object.entries(getRoutes)) { 
+    router.get(path, verifyToken, handlers); 
+}
+
 module.exports = router;
